c60f0dbe9f2dc849c93b4b9657d692282bea3a49
Soutrik <soutrikmaiti.uta@gmail.com>
2019-03-27 10:19:28 -0500

    simulink and MATLAB files added

---
+++ HW5.m
@@ -0,0 +1,22 @@
+clc
+clear all
+close all
+w=0;
+wk=[0.5 1.0 1.5 2.0 2.5 3.0];
+t=0;
+i=0;
+nnin=zeros(1001,3);
+nnout=zeros(1001,1);
+
+for i=1:6
+    w=wk(i);
+    [t,y]=sim('Hw5P1',[0 10]);
+    if i==1
+        nnin=NN_In;
+        nnout=NN_Out;
+    else
+       nnin=[nnin;NN_In];
+       nnout=[nnout;NN_Out];
+    end
+    
+   end
---
+++ P2.m
@@ -0,0 +1,25 @@
+clc
+clear all
+close all
+w=0;
+wk=[0.5 1.0 1.5 2.0 2.5 3.0];
+zk=[0.2 0.4 0.6 0.8 1.0];
+t=0;
+i=0;
+nnin=zeros(1001,4);
+nnout=zeros(1001,1);
+for k=1:5
+  z=zk(k);  
+  for i=1:6
+    w=wk(i);
+    [t,y]=sim('Hw5P2',[0 10]);
+    if i==1 && k==1
+        nnin=NN_In;
+        nnout=NN_Out;
+    else
+       nnin=[nnin;NN_In];
+       nnout=[nnout;NN_Out];
+    end
+    
+   end
+end
